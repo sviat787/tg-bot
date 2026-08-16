676700e7b32392a7c7e6a84368a174383154be31
sviat787 <isviatoslav777@gmail.com>
2026-08-16 09:55:24 +0300

    Update script3.py

--- script3.py
+++ script3.py
@@ -1,3 +1,21 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Bot is alive!'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+Thread(target=run).start()
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types, F
