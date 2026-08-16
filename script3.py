a5c7516aa43dedae75354f342ab1673996454e97
sviat787 <isviatoslav777@gmail.com>
2026-08-16 21:05:00 +0300

    Update script3.py

--- script3.py
+++ script3.py
@@ -233,7 +233,7 @@ async def start_payout(message: Message, state: FSMContext):
         return
 
     await state.set_state(PayoutState.wait_amount)
-    await message.answer(f"Ваш баланс: <b>{user_balance} грн</b>\nВведіть суму для виплати:", parse_mode="HTML")
+    await message.answer(f"Ваш баланс:<b>{user_balance}грн</b>\nВведіть суму для виплати:", parse_mode="HTML")
     @router.message(PayoutState.wait_amount)
     async def process_amount(message: Message, state: FSMContext):
     try:
