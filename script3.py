8b465419ae92de67085ab16890eb2637fcb0d069
sviat787 <isviatoslav777@gmail.com>
2026-08-17 11:05:03 +0300

    Update script3.py

--- script3.py
+++ script3.py
@@ -141,8 +141,8 @@ def get_main_keyboard():
         ],
         resize_keyboard=True
     )
-    @router.message(Command("start"))
-async def cmd_start(message: Message):
+        @router.message(Command("start"))
+        async def cmd_start(message: Message):
     balances = load_balances()
     if str(message.from_user.id) not in balances:
         balances[str(message.from_user.id)] = 0.0
