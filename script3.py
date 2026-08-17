bbe47638ecaf68f8c3ae5c099d044756b955bb40
sviat787 <isviatoslav777@gmail.com>
2026-08-17 09:50:54 +0300

    Update script3.py

--- script3.py
+++ script3.py
@@ -445,6 +445,6 @@ async def main():
     await dp.start_polling(bot)
 
 
-if __name__ == __"__main__":
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
